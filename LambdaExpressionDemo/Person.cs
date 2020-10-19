31be439a68a445dc7a5ac099884a2b9c148951b7
sharyu raut <sharyukraut210298@gmail.com>
2020-10-19 09:33:16 +0530

    [Sharyu] Add. UC1 Add Records to Data Management

---
+++ LambdaExpressionDemo/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LambdaExpressionDemo
+{
+    class Person
+    {
+        public string SSN, Name, Address;
+        public int Age;
+
+        public Person(string ssn, string name, string address, int age)
+        {
+            SSN = ssn;
+            Name = name;
+            Address = address;
+            Age = age;
+        }
+    }
+}
---
+++ LambdaExpressionDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LambdaExpressionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lambda Expressions");
+            //UC 1
+            List<Person> listPersonInCity = new List<Person>();
+            AddRecords(listPersonInCity);
+        }
+
+        //UC1
+        private static void AddRecords(List<Person> listPersonInCity)
+        {
+            listPersonInCity.Add(new Person("aaa", "bbb", "abc", 19));
+            listPersonInCity.Add(new Person("asd", "qwer", "qwsx", 25));
+            listPersonInCity.Add(new Person("qqw", "asdf", "sedr", 61));
+            listPersonInCity.Add(new Person("qre", "zxxv", "wse", 32));
+            listPersonInCity.Add(new Person("wdx", "wsdf", "aws", 10));
+            listPersonInCity.Add(new Person("zse", "asde", "wed", 50));
+        }
+    }
+}
